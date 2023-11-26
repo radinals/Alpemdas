8cb6d5fa2bb1a63573316a6c7bbbacf9d512e46b
Rifkisyah <152040050+Rifkisyah@users.noreply.github.com>
2023-11-26 21:24:01 +0700

    #5 kopi.cpp

--- Tugas_Praktikum #8/#5 kopi.cpp
+++ Tugas_Praktikum #8/#5 kopi.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+void kopi(int step){
+    int gula;
+    
+    cout << "1. Siapkan kopi sachet, air panas, gula, sendok, dan gelas. " << endl;
+    cout << "2. Masukan kopi sachet ke gelas"<< endl;
+    cout << "3. Masukan gula ke gelas berapa sendok ? ";
+    cin >> gula;
+    if(gula < 1){
+        cout << "Kopi Tidak Manis"<<endl;
+    }else if(gula > 3){
+        cout << "gula terlalu manis"<<endl;
+    }else{
+        return;
+    }
+    cout << "3. Masukan air panas secukupnya"<<endl;
+    cout << "4. Aduk gelas yang sudah terisi menggunakan sendok"<<endl;
+    cout << "5. kopi siap disajikan"; 
+}
+
+int main(){
+    int step = 6;
+    
+    cout << "=====||Prosedur membuat kopi sachet||====="<<endl<<endl;
+    
+	kopi(step);
+}
+#include <iostream>
+using namespace std;
+
 void kopi(int procedur, string in){
     for(int i = 0; i < procedur; i++){
     	cout << (i+1) << " ";
