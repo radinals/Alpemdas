6c21731bb8b9e7dddc9b734278830cc4cd66aee7
Rifkisyah <152040050+Rifkisyah@users.noreply.github.com>
2023-11-26 22:29:38 +0700

    #5 kopi.cpp

--- Tugas_Praktikum #8/#5 kopi.cpp
+++ Tugas_Praktikum #8/#5 kopi.cpp
@@ -27,24 +27,3 @@ int main(){
     
 	kopi(step);
 }
-#include <iostream>
-using namespace std;
-
-void kopi(int procedur, string in){
-    for(int i = 0; i < procedur; i++){
-    	cout << (i+1) << " ";
-    	cin >> in;
-	}
-}
-
-int main(){
-    int step;
-    string input;
-    
-    cout << "=====||Prosedur membuat kopi||====="<<endl<<endl;
-    cout << "Ada berapa step by step membuat kopi ? ";
-    cin >> step;
-    cout << "nmasukan step by stepnya : "<< endl;
-    
-	kopi(step,input);
-}
