8b69afe2e5d4ca6cf8d262c95f601ee67709f45b
Rifkisyah <152040050+Rifkisyah@users.noreply.github.com>
2023-11-30 21:32:58 +0700

    Add files via upload

---
+++ Array/Latihan_1.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int max_size;
+    cout << "input max size array : ";
+    cin >> max_size;
+
+    int array[max_size];
+    int index = 0;
+    for(int i = 0; i < max_size; i++){
+        cout << "array index " << index << " : ";
+        cin >> array[i];
+        index++;
+    }
+
+    int sum = 0;
+    for(int i = 0; i < max_size; i++){
+        sum += array[i]; 
+    }
+    cout << "maka penjumlahan kesuluruhan nilai arraynya adalah : " << sum;
+}
